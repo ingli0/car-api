68b1b605de33c71ceda8faa2406c5d9bedff93d1
ingli0 <inbogia@cs.ihu.gr>
2023-09-28 23:10:49 +0300

    made the password hashed

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 import carsRoutes from './routes/cars.js';
 import Car from './models/carModel.js';
-import User from './models/userModel.js'; // Use ES Modules syntax
+import User from './models/userModel.js';  
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import session from 'express-session';
 import mongoose from 'mongoose';
-import ejs from 'ejs'; // Import EJS
+import ejs from 'ejs';  
+import bcrypt from 'bcrypt';
 
 
 
@@ -17,16 +18,18 @@ import ejs from 'ejs'; // Import EJS
 const app = express();
 const PORT = 5000;
 
+const users = []
 
 app.set('view engine', 'ejs');
 
 app.use(session({
-    secret: 'ingli', // Change this to a secure secret
+    secret: 'ingli',  
     resave: false,
     saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.urlencoded({extended: false}))
 
 
 // Configure the local authentication strategy
@@ -48,55 +51,20 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.post('/login',
-    passport.authenticate('local', {
-        successRedirect: '/dashboard',
-        failureRedirect: '/login',
-        failureFlash: true
-    })
-);
-
-app.get('/dashboard', (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.redirect('/login');
-    }
-    res.render('dashboard', { user: req.user });
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
-
+ 
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
-// Route for handling user registration
-app.post('/register', (req, res) => {
-    // Replace with your user registration logic
-    const { username, password } = req.body;
-    // Validate input, hash the password, and store user data in your database
-    // Example: User.create({ username, passwordHash });
-    res.redirect('/login');
-});
-
-app.post('/register', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        // Check if the username already exists
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-        // Create a new user and save it to the database
-        const newUser = new User({ username, password });
-        await newUser.save();
-        res.redirect('/login');
-    } catch (error) {
-        res.status(500).json({ message: 'Registration failed' });
-    }
-});
-
+ 
 
 /* Specify that it will use JSON */
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/cars', carsRoutes);
@@ -105,7 +73,32 @@ app.get('/', (req, res) => res.send('hello from home page'));
 
 app.listen(PORT, () => console.log(`Server is running on port : http://localhost:${PORT}`));
 
+app.post('/register', async (req, res) => {
+    try {
+        console.log('Username:', req.body.username); 
+        console.log('Password:', req.body.password);  
+        console.log('Enail:', req.body.email);  
+        const newUser = new User({
+            username: req.body.username,  
+            password: req.body.password, 
+            email: req.body.email, 
+        });
 
+        
+        const hashedPassword = await bcrypt.hash(newUser.password, 10);
+        newUser.password = hashedPassword;
+
+         
+        await newUser.save();
+
+        // Redirect to the login page after successful registration
+        res.redirect('/login');
+    } catch (error) {
+        // Handle registration errors here
+        console.error(error);
+        res.redirect('/register');
+    }
+});
 
 mongoose.set("strictQuery",false)
 mongoose.connect('mongodb+srv://admin:admin@carapi.d3ibeak.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
--- models/userModel.js
+++ models/userModel.js
@@ -3,36 +3,27 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true }
+    username: {
+        type: String,
+        required: true,
+        unique: true,  
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
 });
-
-// Hash the password before saving to the database
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-        this.password = hashedPassword;
-        next();
-    } catch (error) {
-        next(error);
-    }
-});
-
-// Add a method to compare passwords during authentication
-userSchema.methods.comparePassword = async function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
-};
-
-// Define the serializeUser static method
+ 
 userSchema.statics.serializeUser = function () {
     return function (user, cb) {
         cb(null, user.id);
     };
 };
 
-// Define the deserializeUser static method
 userSchema.statics.deserializeUser = function () {
     return function (id, cb) {
         this.findById(id, (err, user) => {
